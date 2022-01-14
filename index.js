4c2bd915dbe263a43e0fc004337754c72ac99ba7
Peter Kota <kotapeter@gmail.com>
2022-01-14 16:23:10 +0100

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ server.get('/', (req, res) => {
   console.log('RUNTIME_ENV', process.env.RUNTIME)
   res.send('Hello Kinsta')
 })
-const port = process.env.PORT || 3210
+const port = process.env.PORT || 8080
 server.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
