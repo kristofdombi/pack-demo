cfec74ee3ab7132c370b1249508921e651caf696
kristof0425 <dombi.kristof@gmail.com>
2022-05-19 13:49:58 +0200

    feat: add new endpoints

--- index.js
+++ index.js
@@ -2,11 +2,23 @@ const express = require('express')
 
 const server = express()
 
-server.get('/', (req, res) => {
-  console.log('RUNTIME_ENV', process.env.RUNTIME)
+server.get('/', (_, res) => {
+  console.log('RUNTIME env variable', process.env.RUNTIME)
   res.send('Hello Kinsta!')
 })
+
+server.get('/warn', (req, res) => {
+  console.warn(`This is a warning message.\n ${req.ip} - ${req.path}`)
+  res.send('Hello warning!')
+})
+
+server.get('/error', (req, res) => {
+  console.error(`This is an error message.\n ${req.ip} - ${req.path}`)
+  res.send('Hello error!')
+})
+
 const port = process.env.PORT || 8080
+
 server.listen(port, () => {
   console.log('RUNTIME_ENV', process.env.RUNTIME)
   console.log(`Example app listening at http://localhost:${port}`)
